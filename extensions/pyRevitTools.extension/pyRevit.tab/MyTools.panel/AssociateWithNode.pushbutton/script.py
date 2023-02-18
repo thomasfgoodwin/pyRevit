13d9c44fc05a029c1b9c16407cbe2a2afdbcd1f8
thomasfgoodwin <42042237+thomasfgoodwin@users.noreply.github.com>
2023-02-18 13:15:19 -0700

    cleanup

--- extensions/pyRevitTools.extension/pyRevit.tab/MyTools.panel/AssociateWithNode.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/MyTools.panel/AssociateWithNode.pushbutton/script.py
@@ -1,55 +1,29 @@
 
 __title__ = 'Associate a family with a Graphviz node'
 from Autodesk.Revit import DB
-
-
+from pyrevit import script
+import json
+import sys
+sys.path.append('C:\Program Files\Graphviz')
+import graphviz
 doc = __revit__.ActiveUIDocument.Document
 selection = __revit__.ActiveUIDocument.Selection
 
-selection = [doc.GetElement(x) for x in selection.GetElementIds()]
+selection_ids = [x for x in selection.GetElementIds()]
 
-t = DB.Transaction(doc, __title__)
-t.Start()
-for x in selection:
-    node_parameter = x.LookupParameter('Node')
-    print(node_parameter.AsString())
-    node_parameter.Set('Set by script')
-    print(node_parameter.AsString())
-    for p in x.Parameters:
-        if p.IsShared:
-            print(p.Definition.Name, p.Id)
-t.Commit()
-
-# Creating collector instance and collecting all the walls from the model
-wall_collector = DB.FilteredElementCollector(doc)\
-                   .OfCategory(DB.BuiltInCategory.OST_Walls)\
-                   .WhereElementIsNotElementType()
+data = {}
+for item_id in selection_ids:
+    element = doc.GetElement(item_id)
+    unique_id = element.UniqueId
+    # parameter = element.LookupParameter('Node')
+    parameter = DB.BuiltInParameter.ALL_MODEL_MARK
+    mark = element.get_Parameter(parameter).AsString()
+    data[unique_id] = 'Set by script'
 
 
-# Iterate over wall and collect Volume data
-total_volume = 0.0
-selected_volume = 0.0
+file = script.get_document_data_file('node_table', 'json', add_cmd_name=False)
+print(file)
+print(data)
+with open(file, 'w') as write_file:
+         json.dump(data, write_file)
 
-for wall in wall_collector:
-    vol_param = wall.Parameter[DB.BuiltInParameter.HOST_VOLUME_COMPUTED]
-    if vol_param:
-        total_volume = total_volume + vol_param.AsDouble()
-
-
-for wall in selection:
-    vol_param = wall.Parameter[DB.BuiltInParameter.HOST_VOLUME_COMPUTED]
-    if vol_param:
-        selected_volume = selected_volume + vol_param.AsDouble()
-
-# now that results are collected, print the total
-print("Total Volume of selected walls in model is: {}".format(selected_volume))
-
-print("Total Volume of all walls in model is: {}".format(total_volume))
-
-
-"""from Autodesk.Revit.DB import *
-outlet_ID = ElementId(1109535)
-outlet = doc.GetElement(outlet_ID)
-print(list(outlet.Parameters))
-for p in outlet.Parameters:
-	print(p.Definition.Name)"""
