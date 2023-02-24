f91aaaf2a5342c6f44fc0d9364a65d087a5a72fc
thomasfgoodwin <42042237+thomasfgoodwin@users.noreply.github.com>
2023-02-23 17:25:14 -0700

    testing

--- extensions/pyRevitTools.extension/pyRevit.tab/MyTools.panel/AssociateWithNode.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/MyTools.panel/AssociateWithNode.pushbutton/script.py
@@ -1,29 +1,35 @@
+#! python3
+import sys
+import clr
+
+import Autodesk.Revit.DB as DB
 
 __title__ = 'Associate a family with a Graphviz node'
-from Autodesk.Revit import DB
+# from Autodesk.Revit import DB
 from pyrevit import script
-import json
-import sys
-sys.path.append('C:\Program Files\Graphviz')
-import graphviz
-doc = __revit__.ActiveUIDocument.Document
-selection = __revit__.ActiveUIDocument.Selection
+# import json
+# import sys
+# import graphviz
+# import graphviz
+# print(help("modules"))
+# doc = __revit__.ActiveUIDocument.Document
+# selection = __revit__.ActiveUIDocument.Selection
 
-selection_ids = [x for x in selection.GetElementIds()]
+# selection_ids = [x for x in selection.GetElementIds()]
 
-data = {}
-for item_id in selection_ids:
-    element = doc.GetElement(item_id)
-    unique_id = element.UniqueId
-    # parameter = element.LookupParameter('Node')
-    parameter = DB.BuiltInParameter.ALL_MODEL_MARK
-    mark = element.get_Parameter(parameter).AsString()
-    data[unique_id] = 'Set by script'
+# data = {}
+# for item_id in selection_ids:
+#     element = doc.GetElement(item_id)
+#     unique_id = element.UniqueId
+#     # parameter = element.LookupParameter('Node')
+#     parameter = DB.BuiltInParameter.ALL_MODEL_MARK
+#     mark = element.get_Parameter(parameter).AsString()
+#     data[unique_id] = 'Set by script'
 
 
-file = script.get_document_data_file('node_table', 'json', add_cmd_name=False)
-print(file)
-print(data)
-with open(file, 'w') as write_file:
-         json.dump(data, write_file)
+# file = script.get_document_data_file('node_table', 'json', add_cmd_name=False)
+# print(file)
+# print(data)
+# with open(file, 'w') as write_file:
+#          json.dump(data, write_file)
 
